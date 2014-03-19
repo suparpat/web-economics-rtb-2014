eaecc20d3d8c17a3f3ffad88335e7a2bfd678204
jmsbrcwll <jmsbrcwll@gmail.com>
2014-03-19 13:53:04 +0000

    added PCA code, but need to un-comment the centered data code

--- Untitled.m
+++ Untitled.m
@@ -12,4 +12,19 @@ for(count=1:10000)
     line = fgets(file);
     %display(thecell);
 end
+the_matrix(~any(isnan(the_matrix),2),:);
+the_matrix(~any(isinf(the_matrix),2),:);
+    y = the_matrix(:,1);
+   features = the_matrix(:,2:10);
+ %for i = 1:size(features, 2)
+  %      meanVal = mean(features(:,i));
+  %     features(:,i) = features(:,i) - meanVal;   
+  %  end
+    %do PCA
+    covariance = (1/8) * features' * features;
+    [V,D] = eig(covariance, 'nobalance');
+    phi = V(:, 1:2);
+    unlabelled_features = features * phi;
+    clear features;
+
 
