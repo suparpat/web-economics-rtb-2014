b9bab2b111d28244da1549a62b1266f38acda2fa
jmsbrcwll <jmsbrcwll@gmail.com>
2014-03-25 12:21:13 +0000

    added feature reading from a line

--- extractFeatures.pl
+++ extractFeatures.pl
@@ -2,7 +2,11 @@ $file = 'data_training_sample.txt';
 open(FILE, $file);
 foreach $line(<FILE>){
 	print"I am reading a line \n";
+	@features = split('\t', $line);
+	foreach(@features){
+		#do something with the features
 
+	}
 
 }
 close(<FILE>);
