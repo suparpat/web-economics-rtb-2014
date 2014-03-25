0981cb251e583ec832958658f41fa195ecbc15d2
jmsbrcwll <jmsbrcwll@gmail.com>
2014-03-25 12:47:21 +0000

    lots of stuff - now writes some of the features in CSV file format

--- extractFeatures.pl
+++ extractFeatures.pl
@@ -1,14 +1,25 @@
 $file = 'data_training_sample.txt';
 open(FILE, $file);
+open(NEWFILE, '>data_training_sample_extracted.txt');
 foreach $line(<FILE>){
-	print"I am reading a line \n";
+	print"$line \n";
 	@features = split('\t', $line);
-	@extracted = ();
-	foreach(@features){
 		#do something with the features
-		
-
-	}
+	@extracted = ();
+	push(@extracted, @features[0]);  #label		
+	push(@extracted, hex(@features[1])); 
+	push(@extracted, @features[2]);
+	push(@extracted, @features[4]);		
+	push(@extracted, @features[6]);
+	push(@extracted, @features[7]);
+	push(@extracted, @features[8]);
+	print  NEWFILE join(',', @extracted);
 
 }
-close(<FILE>);
+foreach (@extracted){
+
+
+	print("label: $_ \n");
+}
+close(FILE);
+close(NEWFILE);
