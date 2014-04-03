8099449369cc88b4d9d836aa524c45f19aff003c
mgrewal1 <mkgrewal51@gmail.com>
2014-04-03 14:07:13 +0100

    Update extractFeatures.pl
    ad slot visibility and position

--- Core/extractFeatures.pl
+++ Core/extractFeatures.pl
@@ -28,6 +28,63 @@ foreach $line(<FILE>){
 	push(@extracted, hex(@features[10])/ (10 **30));  #domain
 	push(@extracted, @features[14]);  #slot width
 	push(@extracted, @features[15]);  #slot height
+	$slot_visibility = @features[16];
+	$slot_visibility_score;
+	if ($slot_visibility == 'Na') {
+		$slot_visibility_score = 0;
+	} 
+	if ($slot_visibility == 'FirstView') {
+		$slot_visibility_score = 1;
+	} 
+	if ($slot_visibility == 'SecondView') {
+		$slot_visibility_score = 2;
+	} 
+	if ($slot_visibility == 'ThirdView') {
+		$slot_visibility_score = 3;
+	} 
+	if ($slot_visibility == 'FourthView') {
+		$slot_visibility_score = 4;
+	} 
+	if ($slot_visibility == 'FifthView') {
+		$slot_visibility_score = 5;
+	} 
+	if ($slot_visibility == 'SixthView') {
+		$slot_visibility_score = 6;
+	} 
+	if ($slot_visibility == 'SeventhView') {
+		$slot_visibility_score = 7;
+	} 
+	if ($slot_visibility == 'EighthView') {
+		$slot_visibility_score = 8;
+	} 
+	if ($slot_visibility == 'NinthView') {
+		$slot_visibility_score = 9;
+	} 
+	if ($slot_visibility == 'TenthView') {
+		$slot_visibility_score = 10;
+	} 
+	if ($slot_visibility == 'OtherView') {
+		$slot_visibility_score = 11;
+	} 
+	push(@extracted, $slot_visibility_score); #ad_slot_visibility
+	$slot_position = @features[17];
+	$slot_position_score;
+	if ($slot_position == 'Na') {
+		$slot_position_score = 0;
+	} 
+	if ($slot_position == 'Fixed') {
+		$slot_position_score = 1;
+	} 
+	if ($slot_position == 'Pop') {
+		$slot_position_score = 2;
+	} 
+	if ($slot_position == 'Background') {
+		$slot_position_score = 3;
+	} 
+	if ($slot_position == 'Float') {
+		$slot_position_score = 4;
+	} 
+	push(@extracted, $slot_position_score); #ad_position
 	print  NEWFILE join(',', @extracted) . "\n";
 
 }
